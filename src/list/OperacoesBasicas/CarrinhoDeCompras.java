3961794b27d235a6994ffb6412d0dc662b24e238
Higor Bezerra Tomaz <higordesign@hotmail.com>
2024-08-26 12:19:30 -0300

    Desafio Resolvid
    Signed-off-by: Higor Bezerra Tomaz <higordesign@hotmail.com>

---
+++ src/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,82 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    //atributos
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem( String nome, double preco, int quantidade) {
+        Item item = new Item(nome, preco, quantidade);
+        this.itemList.add(item);
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itemsParaRemover = new ArrayList<>();
+        if (!itemList.isEmpty()) {
+            for (Item i : itemList) {
+                if (i.getNome().equalsIgnoreCase(nome)) {
+                    itemsParaRemover.add(i);
+                }
+            }
+            itemList.removeAll(itemsParaRemover);
+        } else {
+            System.out.println("A lista está vazia !");
+        }
+    }
+
+    public double CalcularPrecoValorTotal() {
+        double valorTotal = 0d;
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                double valorItem = item.getPreco() * item.getQuantidade();
+                valorTotal += valorItem;
+            }
+            return valorTotal;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirItens() {
+        if (!itemList.isEmpty()) {
+            System.out.println(this.itemList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+    @Override
+    public String toString() {
+        return "CarrinhoDeCompras{" +
+                "itens=" + itemList +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        //criando uma instância do carrinho de compras
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        //adicionando itens ao carrinho
+        carrinhoDeCompras.adicionarItem("Açucar", 2.50, 3);
+        carrinhoDeCompras.adicionarItem("café", 5.50,4);
+        carrinhoDeCompras.adicionarItem("leite", 4.80, 5);
+        carrinhoDeCompras.adicionarItem("Ovo", 2.80, 6);
+
+        //Exibindo os itens no carrinho
+        carrinhoDeCompras.exibirItens();
+
+        //Removendo um item do carrinho
+        carrinhoDeCompras.removerItem("Açucar");
+
+        //Exibir itens carrinho
+        carrinhoDeCompras.exibirItens();
+
+        //Calculando e exibindo o valor total da compra
+        System.out.println("O valor total da compra é= " + carrinhoDeCompras.CalcularPrecoValorTotal());
+    }
+}
---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,59 @@
+package list.OperacoesBasicas;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListaTarefa {
+
+    //atributo
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa (String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricaoTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        //abaixo estamos imprimindo o número de tarefas existentes na lista.
+        //que retorna um array vazio pois não  tem nenhuma tarefa adicionada.
+        System.out.println("O numero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        //abaixo estamos adicionando tarefas.
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+
+        //aqui estamos imprimindo a quantidade de tarefas existentes.
+        System.out.println("O número total de eleentos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("tarefa 1");
+        System.out.println("O número total de eleentos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricaoTarefas();
+        System.out.println();
+    }
+}
